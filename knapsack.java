05a2f11614733e212aa3691f7a0393e2ec431594
MChang360686 <readingcyclist@gmail.com>
2023-10-02 14:59:04 -0400

    chose java as language

---
+++ knapsack.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class knapsack{
+
+    /*
+     * We are given some objects and their values
+     * 
+     * We want to solve this using Dynamic Programming with
+     * a bottom up approach
+     * 
+     * 
+     */
+
+
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter a number to set knapsack capacity");
+    Integer capacity = scan.nextInt();
+    System.out.println(capacity);
+    scan.close();
+    }
+}
