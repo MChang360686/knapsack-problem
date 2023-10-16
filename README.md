fd1d7e8ce60dc9ad8fa7db9c26b5bf7108d19698
MChang360686 <readingcyclist@gmail.com>
2023-10-16 19:27:41 -0400

    Added array

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ We have x amount of goods weighing y kilos each.  We have a bag/box that can onl
 
 # Solution
 We want to solve this problem with Dynamic Programming.  In the video, Gavin Lon details finding the solution by creating a 2d array and exploiting memoization
+
+Memoization is the technique of optimizing our application by computing and storing the results we want in a "cache"[1].  In the case of the knapsack problem, Gavin Lon uses a 2D array to precompute and store the solution.  
+
+Additionally, I also wanted to make this a command line interface.
+
+Consider the 2D array as a graph, with an X and Y axis.  The X axis is the weight, the Y axis the cost, and we add the value downwards.
+
+1. https://www.freecodecamp.org/news/memoization-in-javascript-and-react/#:~:text=In%20programming%2C%20memoization%20is%20an,instead%20of%20computing%20it%20again.
--- knapsack.java
+++ knapsack.java
@@ -7,17 +7,22 @@ public class knapsack{
      * We are given some objects and their values
      * 
      * We want to solve this using Dynamic Programming with
-     * a bottom up approach
+     * a bottom up approach.  Additionally, we want to use memoization
      * 
      * 
      */
 
-
     public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Please enter a number to set knapsack capacity");
-    Integer capacity = scan.nextInt();
-    System.out.println(capacity);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number of items to compare");
+        Integer numItems = scan.nextInt();
+        System.out.println("Please enter a number to set knapsack capacity");
+        Integer capacity = scan.nextInt();
+        System.out.println(capacity);
+
+        Integer[][] arr = new Integer[numItems][capacity];
+        System.out.println(arr);
+
     scan.close();
     }
 }
